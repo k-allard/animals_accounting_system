2524a4c5030b3151edf77b5af35c079cd1ee62e1
kallard <kallard@student.21-school.ru>
2021-08-10 02:04:44 +0300

    Delete debug print

--- src/main/java/com/accounting/Main.java
+++ src/main/java/com/accounting/Main.java
@@ -11,13 +11,6 @@ public class Main {
             System.exit(1);
         }
         ArrayList<List<String>> rules = CSVParser.parseFile("task.csv");
-
-        System.out.println("\nЖивотные:");
-        System.out.println(animals);
-        System.out.println("\nЗадачи:");
-        System.out.println(rules);
-        System.out.println();
-
         for (List<String> ruleSet : rules) {
             int result = 0;
             for (List<String> animal : animals) {
