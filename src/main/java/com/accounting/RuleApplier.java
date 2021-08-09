b5cba1a6cbcf616a6e2a7756317a9cc8aede45cd
kallard <kallard@student.21-school.ru>
2021-08-10 01:44:58 +0300

    Add RuleApplier class

---
+++ src/main/java/com/accounting/RuleApplier.java
@@ -0,0 +1,34 @@
+package com.accounting;
+
+import java.util.List;
+
+public class RuleApplier {
+
+    static boolean applySingleRule(List<String> animal, String rule) {
+        for (String animalFeature : animal) {
+            if (animalFeature.equals(rule))
+                return true;
+        }
+        return false;
+    }
+
+    static boolean applyRulesSet(List<String> animal, List<String> rulesSet) {
+        boolean result = false;
+        for (String rule : rulesSet) {
+            if (!rule.contains(" ")) {
+                result = applySingleRule(animal, rule);
+            } else if (rule.substring(0, 3).equals("НЕ ")) {
+                result = !applySingleRule(animal, rule.substring(3));
+            } else if (rule.contains(" ИЛИ ")) {
+                String[] twoRulesArray = rule.split(" ");
+                result = (applySingleRule(animal, twoRulesArray[0]) || applySingleRule(animal, twoRulesArray[2]));
+            } else {
+                System.out.println("task.csv: Некорректное правило");
+                System.exit(1);
+            }
+            if (!result)
+                break;
+        }
+        return result;
+    }
+}
