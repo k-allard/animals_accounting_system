ca359fcac1860a863daad18d1deafe6c9fae9832
kallard <kallard@student.21-school.ru>
2021-08-10 14:43:08 +0300

    Add more applyRulesSet() tests

--- src/test/com/accounting/RuleApplierTest.java
+++ src/test/com/accounting/RuleApplierTest.java
@@ -6,23 +6,35 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RuleApplierTest {
 
     private static final List<String> animal = new ArrayList<>();
-    private static final List<String> rulesSet = new ArrayList<>();
+    private static List<String> rulesSet = new ArrayList<>();
 
     @BeforeAll
     static void init() {
         animal.add("ВОДОПЛАВАЮЩЕЕ");
         animal.add("ПОЛОСАТОЕ");
         animal.add("КОРОТКОХВОСТОЕ");
-        rulesSet.add("НЕ КОРОТКОХВОСТОЕ");
     }
 
     @Test
     void applyRulesSet() {
-        boolean result = RuleApplier.applyRulesSet(animal, rulesSet);
-        assertFalse(result);
+        rulesSet.add("ПОЛОСАТОЕ");
+        rulesSet.add("НЕ КОРОТКОХВОСТОЕ");
+        assertFalse(RuleApplier.applyRulesSet(animal, rulesSet));
+        rulesSet.clear();
+        rulesSet.add("КОРОТКОХВОСТОЕ ИЛИ БЕСХВОСТОЕ");
+        rulesSet.add("НЕ СУХОПУТНОЕ");
+        assertTrue(RuleApplier.applyRulesSet(animal, rulesSet));
+        rulesSet.clear();
+        rulesSet.add("ПОЛОСАТОЕ ИЛИ ОДНОТОННОЕ");
+        assertTrue(RuleApplier.applyRulesSet(animal, rulesSet));
+        rulesSet.clear();
+        rulesSet.add("ВОДОПЛАВАЮЩЕЕ");
+        rulesSet.add("НЕ ПОЛОСАТОЕ");
+        assertFalse(RuleApplier.applyRulesSet(animal, rulesSet));
     }
 }
