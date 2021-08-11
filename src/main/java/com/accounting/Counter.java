1e81f529103be7cb237528ad56343ed969778a16
kallard <kallard@student.21-school.ru>
2021-08-12 00:36:12 +0300

    Rename Counter class

--- src/main/java/com/accounting/Counter.java
+++ src/main/java/com/accounting/Counter.java
@@ -8,34 +8,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class AnimalCounter {
-
-//    static class CounterThread implements Runnable {
-//        private Map<Integer, Integer> result;
-//        Map<Integer, Rule> rules;
-//        ArrayList<List<String>> animals;
-//
-//        public CounterThread(Map<Integer, Rule> rules, ArrayList<List<String>> animals) {
-//            this.rules = rules;
-//            this.animals = animals;
-//            result = new HashMap<>();
-//        }
-//
-//        public void run() {
-//            for (Integer ruleId : rules.keySet()) {
-//                int res = 0;
-//                for (List<String> animal : animals) {
-//                    if (RuleApplier.applyRulesSet(animal, rules.get(ruleId)))
-//                        res++;
-//                }
-//                result.put(ruleId, res);
-//            }
-//        }
-//
-//        public Map<Integer, Integer> getResult() {
-//            return this.result;
-//        }
-//    }
+public class Counter {
 
     static Map<Integer, Integer> addMapToMap(Map<Integer, Integer> one, Map<Integer, Integer> two) {
         Map<Integer, Integer> res = new HashMap<>();
