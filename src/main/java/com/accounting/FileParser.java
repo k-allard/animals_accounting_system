84d9ef931f9b8eecab1ea8ec27b6fdbd2c2a5395
kallard <kallard@student.21-school.ru>
2021-08-12 00:39:03 +0300

    Pass THREADPOOLSIZE and MAXLINE as parametres from main

--- src/main/java/com/accounting/FileParser.java
+++ src/main/java/com/accounting/FileParser.java
@@ -7,8 +7,6 @@ import java.util.*;
 
 public class FileParser {
 
-    private static final int MAXLINE = 25;
-
     static Map<Integer, Rule> parseRuleFile(String fileName) throws IOException {
         Map<Integer, Rule> result = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
@@ -22,41 +20,14 @@ public class FileParser {
         return result;
     }
 
-    static ArrayList<List<String>> getLinesFromFile(BufferedReader br) throws IOException {
+    static ArrayList<List<String>> getLinesFromFile(BufferedReader br, int maxNumOfLines) throws IOException {
         int i = 0;
         String line;
         ArrayList<List<String>> result = new ArrayList<>();
-        while (i < MAXLINE && (line = br.readLine()) != null) {
+        while (i < maxNumOfLines && (line = br.readLine()) != null) {
             result.add(Arrays.asList(line.split(",")));
             i++;
         }
         return result;
     }
-
-//    static void parseAndCountAnimals(String fileName, Map<Integer, Rule> rules) throws IOException {
-//        Map<Integer, Integer> result = initMap(rules);
-//        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-//            ArrayList<List<String>> animals = getLinesFromFile(br);
-//            while (!animals.isEmpty()) {
-//                ExecutorService executor = Executors.newFixedThreadPool(THREADPOOLSIZE);
-//                ConcurrentLinkedQueue<CounterThread> threads = new ConcurrentLinkedQueue<>();
-//                while (threads.size() < THREADPOOLSIZE && !animals.isEmpty()) {
-//                    CounterThread thread = new CounterThread(rules, animals);
-//                    threads.add(thread);
-//                    executor.execute(thread);
-//                    animals = getLinesFromFile(br);
-//                }
-//                executor.shutdown();
-//                while (!executor.isTerminated()) { }
-//                while (threads.size() > 0) {
-//                    CounterThread thread = threads.poll();
-//                    Map<Integer, Integer> res = thread.getResult();
-//                    result = addMapToMap(result, res);
-//                }
-//            }
-//        }
-//        for (Integer id : rules.keySet()) {
-//            System.out.println(id + ". Правилу " + rules.get(id).rule + " соответствует " + result.get(id) + " животных");
-//        }
-//    }
 }
--- src/main/java/com/accounting/Main.java
+++ src/main/java/com/accounting/Main.java
@@ -5,8 +5,14 @@ import java.util.Map;
 
 public class Main {
 
+    public static final int THREADPOOLSIZE = 6;
+    public static final int MAXLINE = 25;
+
     public static void main(String[] args) throws IllegalArgumentException, IOException {
         Map<Integer, Rule> rules = FileParser.parseRuleFile("task.csv");
-        AnimalCounter.countAnimals("animals.csv", rules);
+        Map<Integer, Integer> result = Counter.countAnimals("animals.csv", rules, THREADPOOLSIZE, MAXLINE);
+        for (Integer id : rules.keySet()) {
+            System.out.println(id + ". Правилу " + rules.get(id).rule + " соответствует " + result.get(id) + " животных");
+        }
     }
 }
