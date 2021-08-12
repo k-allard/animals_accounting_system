1a54e10d7ee2076af409865e364a1d016832e15a
kallard <kallard@student.21-school.ru>
2021-08-12 19:51:36 +0300

    Measure performance: 2,2--3 sec for 1 million animals

--- src/main/java/com/accounting/Main.java
+++ src/main/java/com/accounting/Main.java
@@ -5,15 +5,18 @@ import java.util.Map;
 
 public class Main {
 
-    public static final int THREADPOOLSIZE = 6;
-    public static final int MAXLINE = 25;
+    public static final int THREADPOOLSIZE = 5;
+    public static final int MAXLINE = 1000;
 
     public static void main(String[] args) throws IllegalArgumentException, IOException {
+        long startTime = System.currentTimeMillis();
         Map<Integer, Rule> rules = FileParser.parseRuleFile("task.csv");
-        Map<Integer, Integer> result = Counter.countAnimals("animals.csv", rules, THREADPOOLSIZE, MAXLINE);
+        Map<Integer, Integer> result = Counter.countAnimals("animals.1000000.csv", rules, THREADPOOLSIZE, MAXLINE);
+        long endTime = System.currentTimeMillis();
         for (Map.Entry<Integer, Rule> ruleEntry: rules.entrySet()) {
             System.out.println(ruleEntry.getKey() + ". Правилу " + ruleEntry.getValue().rule +
                     " соответствует " + result.get(ruleEntry.getKey()) + " животных");
         }
+        System.out.println("Время: [" + (endTime - startTime) + "] миллисекунд");
     }
 }
